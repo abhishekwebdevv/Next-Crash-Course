6d12392d8624b05d81b4cf59e936be407ce53afb
sageOMEGA <abhishekdhakad61@gmail.com>
2021-06-19 12:53:46 +0530

    Layout

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function about() {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>About Page</h1>
+    </div>
+  );
+}
